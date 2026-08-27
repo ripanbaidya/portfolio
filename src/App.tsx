cb092ff7bfa4f7bddc616ec417e0f21c158f7c4a
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-08-27 22:30:21 +0530

    Add Blog section routing to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from "./components/Container";
 import { Navbar } from "./components/Navbar";
 import { ScrollToTopButton } from "./components/ScrollToTopButton";
+import { Blog } from "./sections/Blog";
 import { Contact } from "./sections/Contact";
 import { Experience } from "./sections/Experience";
 import { Footer } from "./sections/Footer";
@@ -10,12 +11,26 @@ import { TechnologyMarquee } from "./sections/TechnologyMarquee";
 import { Uses } from "./sections/Uses";
 
 export default function App() {
+  
   if (window.location.pathname === "/uses") {
     return (
       <div className="flex min-h-screen flex-col bg-neutral-950 font-sans text-neutral-100">
         <Navbar />
         <main className="flex-1 bg-neutral-950">
           <Uses />
+        </main>
+        <Footer />
+        <ScrollToTopButton />
+      </div>
+    );
+  }
+
+  if (window.location.pathname === "/blog") {
+    return (
+      <div className="flex min-h-screen flex-col bg-neutral-950 font-sans text-neutral-100">
+        <Navbar />
+        <main className="flex-1 bg-neutral-950">
+          <Blog />
         </main>
         <Footer />
         <ScrollToTopButton />
